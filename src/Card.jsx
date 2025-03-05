1bf87b563d6730de43bb1eaf9078d12ab25fe298
johan <johansanchezvalencia@gmail.com>
2025-03-04 20:34:06 -0500

    creacion de las tres cards

---
+++ src/Card.jsx
@@ -0,0 +1,15 @@
+
+
+function Card() {
+    return (
+        <section className="card">
+            <div></div>
+            <div>
+                <p>Ver</p>
+            </div>
+        </section>
+    )
+}
+
+
+export default Card;
---
+++ src/MenuLateral.jsx
@@ -0,0 +1,21 @@
+import userLogo from './assets/logo1.jpg'
+
+function MenuLateral() {
+    return (
+        <aside className="panel-control-aside">
+            <div>
+                <img src={userLogo} alt="" />
+            </div>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </aside>
+    );
+}
+
+export default MenuLateral;
